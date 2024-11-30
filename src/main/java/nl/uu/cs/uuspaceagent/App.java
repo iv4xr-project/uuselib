457c2484b3031ef4e9d38d42b04fdb46090aa08a
Prasetya, S.W.B. (Wishnu) <s.w.b.prasetya@uu.nl>
2024-11-30 12:33:16 +0100

    intial commit

---
+++ src/main/java/nl/uu/cs/uuspaceagent/App.java
@@ -0,0 +1,13 @@
+package nl.uu.cs.uuspaceagent ;
+
+import eu.iv4xr.framework.spatial.*;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+    	var v = new Vec3(1,2,3) ;
+        System.out.println("Hello World!  " + v);
+    }
+}
