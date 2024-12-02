ee93923ee668814bdf568f5417da68241ef81b7f
Charl-Pierre Marais <charlpierre0301@gmail.com>
2024-12-02 17:00:29 +0100

    started work on first new test

---
+++ src/test/java/nl/uu/cs/uuspaceagent/construction/Test_ComplexPathfinding.java
@@ -0,0 +1,101 @@
+package nl.uu.cs.uuspaceagent.construction;
+
+import eu.iv4xr.framework.mainConcepts.TestAgent;
+import eu.iv4xr.framework.mainConcepts.TestDataCollector;
+import eu.iv4xr.framework.spatial.Vec3;
+import nl.uu.cs.aplib.mainConcepts.Goal;
+import nl.uu.cs.aplib.mainConcepts.GoalStructure;
+import nl.uu.cs.aplib.utils.Pair;
+import nl.uu.cs.uuspaceagent.DPos3;
+import nl.uu.cs.uuspaceagent.TestUtils;
+import nl.uu.cs.uuspaceagent.UUSeAgentState;
+import nl.uu.cs.uuspaceagent.UUTacticLib;
+
+import org.junit.jupiter.api.Test;
+import uuspaceagent.*;
+
+import static nl.uu.cs.aplib.AplibEDSL.* ;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static nl.uu.cs.uuspaceagent.PrintInfos.showWOMAgent;
+import static nl.uu.cs.uuspaceagent.TestUtils.console;
+import static nl.uu.cs.uuspaceagent.TestUtils.loadSE;
+
+public class Test_ComplexPathfinding {
+
+    /**
+     * Auto-navigate to a given point.
+     */
+    public Pair<TestAgent,GoalStructure> deployAgent(Vec3 destination) throws InterruptedException {
+        console("*** start test...") ;
+        var agentAndState = loadSE("islanddswithdoors") ;
+        TestAgent agent = agentAndState.fst ;
+        UUSeAgentState state = agentAndState.snd ;
+        Thread.sleep(1000);
+        state.updateState(state.agentId);
+
+        // agent start location should be around: <10.119276,-5.0025,55.681934>
+        //  orientationForward: <-0.043967947,-2.0614608E-4,0.9990329> ... so looking towards z-axis
+        console(showWOMAgent(state.worldmodel));
+
+        var sqAgent = state.navgrid.gridProjectedLocation(state.worldmodel.position) ;
+        var sqDestination = state.navgrid.gridProjectedLocation(destination) ;
+        var centerOfSqDestination = state.navgrid.getSquareCenterLocation(sqDestination) ;
+
+        //float dth = 1.3f * Grid2DNav.SQUARE_SIZE ;
+        //final float distance_to_sq_threshold = dth*dth ;
+
+        GoalStructure G = goal("close to destination")
+                .toSolve((Pair<Vec3,Vec3> positionAndOrientation) -> {
+                    //var currentAgentSq = st.grid2D.gridProjectedLocation(st.wom.position) ;
+                    //return currentAgentSq.equals(sqDestination) ;
+                    var pos = positionAndOrientation.fst ;
+                    return Vec3.sub(centerOfSqDestination,pos).lengthSq() <= UUTacticLib.THRESHOLD_SQUARED_DISTANCE_TO_SQUARE ;
+                })
+                .withTactic(UUTacticLib.navigateToTAC(destination))
+                .lift() ;
+
+        agent.setGoal(G) ;
+
+        int turn= 0 ;
+        while(G.getStatus().inProgress()) {
+            console(">> [" + turn + "] " + showWOMAgent(state.worldmodel));
+            agent.update();
+            //Thread.sleep(50);
+            turn++ ;
+            if (turn >= 1400) break ;
+        }
+
+        //SocketReaderWriterKt.closeIfCloseable(state.env().getController());
+        TestUtils.closeConnectionToSE(state);
+        test_Goal(agent, state, G) ;
+        return new Pair<>(agent,G) ;
+    }
+
+    public void test_Goal(TestAgent agent, UUSeAgentState state, GoalStructure G) throws InterruptedException {
+        agent.setGoal(G) ;
+        int turn= 0 ;
+        while(G.getStatus().inProgress()) {
+            //console(">> [" + turn + "] " + showWOMAgent(state.wom));
+            agent.update();
+            //Thread.sleep(50);
+            turn++ ;
+            if (turn >= 1400) break ;
+        }
+        //closeIfCloseable(state.env().getController());f
+        TestUtils.closeConnectionToSE(state);
+    }
+
+    @Test
+    public void test_navigate_and_grind() throws InterruptedException {
+        // This is a position that is unreachable, so this goal should abort
+        console("*** start test...") ;
+        Vec3 dest = new Vec3(43,1.25f,7.5f) ;
+        var agent_and_goal = deployAgent(dest);
+        TestAgent agent = agent_and_goal.fst ;
+        agent.setTestDataCollector(new TestDataCollector()) ;
+        GoalStructure G = agent_and_goal.snd;
+        G.printGoalStructureStatus();
+        assertTrue(G.getStatus().success());
+        //assertTrue(agent.getTestDataCollector().getNumberOfPassVerdictsSeen() == 2) ;
+    }
+}
